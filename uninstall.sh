719044148afdfc00fbd6a9d4cd0225a1a9bd04c8
mbassiouny33 <mohamed-hisham@hotmail.com>
2020-01-07 00:03:52 +0100

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker stop $(docker ps -qf "name=sdtc") $(docker ps -qf "name=openid_keycloak") $(docker ps -qf "name=nginx") 
-rm -r data
-rm -r uploads
-rm -r docker-compose-letsencrypt-nginx-proxy-companion/nginx-data
-docker system prune -f
-docker volume prune -f
